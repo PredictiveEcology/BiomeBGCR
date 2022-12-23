1cd17804924c5a711e2f565b6a96fc46f5f784fc
Alex Chubaty <achubaty@for-cast.ca>
2022-12-23 16:28:46 -0700

    add binToText()

--- R/utils.R
+++ R/utils.R
@@ -76,3 +76,49 @@ createIOdirs <- function(path) {
   }, logical(1))
   dir.create(file.path(path, "outputs"), recursive = TRUE, showWarnings = FALSE)
 }
+
+#' Converts single floating point binary data into ASCII text data
+#'
+#' @param inDir character string giving path to input data directory; `NA` means use current directory.
+#' @param outDir character string giving path to output data directory; `NA` means use current directory.
+#' @param uniqueSuf character string giving unique suffix.
+#' @param varNo integer. the number of variables (columns); `NA` means all columns.
+#' @param years integer. the number of years of the data (rows); `NA` means all rows.
+#' @param startYear integer. the starting year of the data; `NA` means the first row.
+#'
+#' @return invoked for side effect of converting data files
+#'
+#' @export
+#' @importFrom utils write.table
+binToText <- function(inDir, outDir, uniqueSuf, varNo = NA, years = NA, startYear = NA) {
+  if (!is.na(inDir)) {
+    setwd(inDir)
+  }
+  if (is.na(uniqueSuf)) {
+    binFile <- dir()
+  } else {
+    binFiles <- grep(uniqueSuf, dir(), value = TRUE)
+  }
+  if (!is.na(outDir)) {
+    setwd(outDir)
+  }
+  if (is.na(startYear)) {
+    startYear <- 1
+  }
+  entries <- years * varNo
+
+  for (i in 1:length(binFiles)) {
+    dataMat <- matrix(nrow = years, ncol = varNo + 1)
+    bfile <- file(binFiles[i], "rb")
+    outFile <- file(paste(binFiles[i], "txt", sep = "."), "w")
+    dt <- readBin(bfile, double(), entries, size = 4)
+    for (j in 1:years) {
+      vec <- c(startYear - 1 + j, dt[(j * varNo - (varNo - 1)):(j * varNo)])
+      dataMat[j, ] <- vec
+    }
+    write.table(dataMat, outFile, sep = "\t", row.names = FALSE, col.names = FALSE)
+    close(outFile)
+    close(bfile)
+    print(paste("Finished: ", binFiles[i], ".  FileNo ", i, " of ", length(binFiles), sep = ""))
+  }
+}
